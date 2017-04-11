43c76172b4e6c7e9412ebaade4d2c1dea0323156
cleber <cleber@dronemapp.com>
2017-04-10 21:25:24 -0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,41 @@
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+version = '0.0.1'
+requires = []
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='powerlibs-django-eventful-models',
+    version=version,
+    description="Django models that serve as base for event based actions",
+    long_description=readme,
+    author='Cléber Zavadniak',
+    author_email='cleberman@gmail.com',
+    url='https://github.com/Dronemapp/powerlibs-django-eventful-models',
+    license=license,
+    packages=['powerlibs.django.eventful'],
+    package_data={'': ['LICENSE', 'README.md']},
+    include_package_data=True,
+    install_requires=requires,
+    zip_safe=False,
+    keywords='generic libraries',
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ),
+)
